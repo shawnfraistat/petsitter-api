0243b2653fcf5115e6fa87d7769f69bd30cf93b7
shawnfraistat <shawn.fraistat@gmail.com>
2019-01-13 10:54:28 -0500

    Remove Redundant Pic_Url From Client Resource
    Pic_url removed from client resource, as it's already associated with
    the client's user account

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_155016) do
+ActiveRecord::Schema.define(version: 2019_01_13_155215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clients", force: :cascade do |t|
     t.text "about"
-    t.string "pic_url"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
