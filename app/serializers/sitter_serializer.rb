0843a94089f4cbc8000a1d22f3179fd9e42e5081
shawnfraistat <shawn.fraistat@gmail.com>
2019-01-16 11:55:45 -0500

    Add Name Field to Sitter
    Added a name field to sitter, so that their names can be displayed to
    users.

--- app/serializers/sitter_serializer.rb
+++ app/serializers/sitter_serializer.rb
@@ -1,5 +1,5 @@
 class SitterSerializer < ActiveModel::Serializer
-  attributes :id, :about, :price, :service_range, :animal_types, :services_provided
+  attributes :id, :about, :price, :service_range, :animal_types, :services_provided, :name
   belongs_to :user
   # has_many :favorites
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_014640) do
+ActiveRecord::Schema.define(version: 2019_01_16_165453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_01_16_014640) do
     t.string "services_provided"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_sitters_on_user_id"
   end
 
