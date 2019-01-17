58c72bc053a00dbb4c752f7ef00296053ad987ee
shawnfraistat <shawn.fraistat@gmail.com>
2019-01-17 18:39:57 -0500

    Tried to Fix Cors Bug
    Getting a cors error on deployed site. Seeing if manually changing
    cors.rb fixes it.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 # Read more: https://github.com/cyu/rack-cors
 
 if Rails.env.development?
-  development_client_origin = "http://localhost:7165"
+  development_client_origin = "https://shawnfraistat.github.io/petsitter-client"
 end
 
 development_client_origin ||= ENV['CLIENT_ORIGIN']
