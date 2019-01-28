fd2782108691a1285bbd8ea9abfa62019972bca2
shawnfraistat <shawn.fraistat@gmail.com>
2019-01-28 14:42:22 -0500

    Add Read Column to Message Resource
    Want to keep track of whether an individual message has been read,
    so added a 'read' boolean to the message resource.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_172211) do
+ActiveRecord::Schema.define(version: 2019_01_28_193921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_01_28_172211) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read"
     t.index ["exchange_id"], name: "index_messages_on_exchange_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
